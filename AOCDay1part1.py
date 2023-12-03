f446afc8abc26b31cfa0b99876af36bfff482ae8
iopredman <iopredman@gmail.com>
2023-12-03 15:45:46 -0500

    Update and rename AOCDay1.py to AOCDay1part1.py

--- AOCDay1part1.py
+++ AOCDay1part1.py
@@ -1,5 +1,3 @@
-# set AOC_SESSION=53616c7465645f5fa0936830c57b4acaf3a7ee8d899e6108bf28e26cb66329eb2347017d9b2b824175323f06addfba173b99283d716ed00ddea5e7453f74b401
-
 from aocd import get_data
 lines = get_data(day=1, year=2023).split("\n")
 
